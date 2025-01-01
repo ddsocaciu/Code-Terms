9cd6d7133e77db90ebbf6d23b4a2b481735a0043
Ace1210 <mmahrhofer@student.tgm.ac.at>
2025-01-01 20:27:24 +0100

    Change Class

--- SourceCode/src/GameMode/GameController.java
+++ SourceCode/src/GameMode/GameController.java
@@ -27,13 +27,7 @@ public class GameController {
             // Fenster erstellen
             if (gameModel != null) {
                 JFrame frame = new JFrame("Hangman Game");
-                GamePanel gamePanel = null;
-                try {
-                    gamePanel = new GamePanel(gameModel);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                } finally {
-                }
+                GamePanel gamePanel = new GamePanel(gameModel);
                 frame.add(gamePanel);
                 frame.setSize(400, 300);
                 frame.setLocation(250, 100);
