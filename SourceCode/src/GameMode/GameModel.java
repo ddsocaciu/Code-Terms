d3458d24562244b0831d8b4ce4351dffa55b6a7a
ace0101010 <mmahrhofer@student.tgm.ac.at>
2025-01-02 20:48:22 +0100

    Die Klassen logik angepasst sodass nun die neue Methode loadFragen verwendet wird, die die Fragen und Antworten aus dem Default Pfad holt, es werden nun auch nicht mehr als 50 Fragen/Antworten erlaubt.

--- SourceCode/src/GameMode/GameModel.java
+++ SourceCode/src/GameMode/GameModel.java
@@ -1,13 +1,12 @@
 package GameMode;
 
 import FileSource.FileLoader;
-
 import java.awt.*;
-import java.io.*;
+import java.io.IOException;
 import java.util.Random;
 
 /**
- * Diese Klasse stellt die Logik des Hangman Games dar
+ * Diese Klasse stellt die Logik des Hangman Games dar.
  *
  * @author Maximilian Mahrhofer
  * @version 2024-12-27
@@ -18,59 +17,50 @@ public class GameModel {
     private String[][] questions;
     private String[] currentQA;
     private int questionCount;
-    FileLoader fileLoader = new FileLoader();
+    private final FileLoader fileLoader = new FileLoader();
 
     public GameModel() {
-        this.questions = new String[100][2]; // Maximale Anzahl von Fragen (100)
-        this.currentQA = new String[2]; // Initialisierung für eine Frage
-        this.questionCount = 0; //  Frageanzahl
+        this.questions = new String[2][50]; // Maximale Anzahl von Fragen (50).
+        this.currentQA = new String[2];    // Initialisierung für eine Frage.
+        this.questionCount = 0;           // Anzahl der geladenen Fragen.
     }
 
-    // Lade Fragen aus der Datei
+    // Lade Fragen aus einer Datei.
     public void loadQuestions(String filePath) throws IOException {
-
-        //questions = fileLoader.loadQuestions(filePath);
+        questions = fileLoader.loadFragen(filePath);
         questionCount = 0;
 
-
-        for (String[] question : questions) {
-            if (question[0] != null && question[1] != null) {
+        for (int i = 0; i < questions[0].length; i++) {
+            if (questions[0][i] != null && questions[1][i] != null) {
                 questionCount++;
             }
         }
-
 
         if (questionCount == 0) {
             throw new IOException("Keine Fragen gefunden oder Fehler beim Laden der Fragen.");
         }
     }
 
-
-
-    // Zufällige Frage auswählen
+    // Wählt eine zufällige Frage aus.
     public void selectRandomQuestion() {
-        // Sicherstellen, dass Fragen existieren
         if (questionCount == 0) {
             currentQA = new String[]{"Keine Frage Verfügbar", ""};
             return;
         }
 
-        // Zufällige Auswahl einer Frage
         Random rand = new Random();
-        currentQA = questions[rand.nextInt(questionCount)]; // Frage aus den geladenen Fragen auswählen
+        int index = rand.nextInt(questionCount);
+        currentQA = new String[]{questions[0][index], questions[1][index]};
     }
 
-    // Frage abrufen
     public String getCurrentQuestion() {
         return currentQA != null && currentQA[0] != null ? currentQA[0] : "Keine Frage Verfügbar";
     }
 
-    // Antwort abrufen
     public String getCurrentAnswer() {
         return currentQA != null && currentQA[1] != null ? currentQA[1].toLowerCase() : "";
     }
 
-    // Fehlerhafte Versuche erhöhen
     public int getWrongGuesses() {
         return wrongGuesses;
     }
@@ -81,30 +71,27 @@ public class GameModel {
         }
     }
 
-    // Spiel beendet?
     public boolean isGameOver() {
         return wrongGuesses >= 6;
     }
 
-    // Spiel zurücksetzen
     public void resetGame() {
         wrongGuesses = 0;
         selectRandomQuestion();
     }
 
-    // Zeichnen des Spiels (hängende Person)
     public void draw(Graphics g) {
         g.setColor(color);
-        g.drawLine(50, 200, 150, 200); // Zeichnet den Boden
-        g.drawLine(100, 200, 100, 50); // Zeichnet die Stange
-        g.drawLine(100, 50, 200, 50);  // Zeichnet die obere Basis
-        g.drawLine(200, 50, 200, 70);  // Zeichnet die Schlinge
+        g.drawLine(50, 200, 150, 200); // Zeichnet den Boden.
+        g.drawLine(100, 200, 100, 50); // Zeichnet die Stange.
+        g.drawLine(100, 50, 200, 50);  // Zeichnet die obere Basis.
+        g.drawLine(200, 50, 200, 70);  // Zeichnet die Schlinge.
 
-        if (wrongGuesses > 0) g.drawOval(185, 70, 30, 30); // Der Kopf
-        if (wrongGuesses > 1) g.drawLine(200, 100, 200, 150); // Der Körper
-        if (wrongGuesses > 2) g.drawLine(200, 110, 180, 130); // Linker Arm
-        if (wrongGuesses > 3) g.drawLine(200, 110, 220, 130); // Rechter Arm
-        if (wrongGuesses > 4) g.drawLine(200, 150, 180, 180); // Linkes Bein
-        if (wrongGuesses > 5) g.drawLine(200, 150, 220, 180); // Rechtes Bein
+        if (wrongGuesses > 0) g.drawOval(185, 70, 30, 30); // Der Kopf.
+        if (wrongGuesses > 1) g.drawLine(200, 100, 200, 150); // Der Körper.
+        if (wrongGuesses > 2) g.drawLine(200, 110, 180, 130); // Linker Arm.
+        if (wrongGuesses > 3) g.drawLine(200, 110, 220, 130); // Rechter Arm.
+        if (wrongGuesses > 4) g.drawLine(200, 150, 180, 180); // Linkes Bein.
+        if (wrongGuesses > 5) g.drawLine(200, 150, 220, 180); // Rechtes Bein.
     }
 }
